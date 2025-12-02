5367fd6d034e0ead0ed738caee05bfb694edc257
EmpiricistSkeptic <131171004+EmpiricistSkeptic@users.noreply.github.com>
2025-12-02 17:36:27 +0000

    fix bug

--- api/urls.py
+++ api/urls.py
@@ -11,10 +11,11 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'letters', LetterViewSet, basename='letters')
 
-urlpatterns = router.urls
 
-url_patters = [
+urlpatterns = [
     path("story/init/", StoryInitView.as_view(), name="story-init"),
     path("story/today/", TodayDialogueStartView.as_view(), name="story-today"),
     path("story/answer/", AnswerView.as_view(), name="story-answer"),
 ]
+
+urlpatterns += router.urls
