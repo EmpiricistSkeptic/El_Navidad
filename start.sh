4ecbbd11b1a57d196f1b2efbcb2d83faacd8235c
EmpiricistSkeptic <131171004+EmpiricistSkeptic@users.noreply.github.com>
2026-05-25 23:46:30 +0000

    s

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py import_story \
-  --dialogues api/data/dialogues.json \
-  --letters api/data/letters.json \
+  --dialogues fixtures/dialogues.json \
+  --letters fixtures/letters.json \
   --create-user
 gunicorn project.wsgi:application --bind 0.0.0.0:8000
